da736508d44369320581410c93ff2fcb1eca2a4a
absarrahman <raman040635@gmail.com>
2025-09-23 14:23:59 -0500

    lab04:  starter template
    Signed-off-by: absarrahman <raman040635@gmail.com>

---
+++ fall_25/lab04/sample_app/browser.py
@@ -0,0 +1,25 @@
+
+# import required stuff
+
+class Browser:
+    def __init__(self):
+      # initialize linked list and current site index
+      pass
+
+    def navigate_to(self, url):
+      # you need to iterate from site and remove the urls
+      # after that insert and increment your index
+      pass
+
+    def forward(self):
+      # increment index and make sure you are handling corner cases
+      pass
+
+    def back(self):
+      # increment index and make sure you are handling corner cases
+      pass
+
+
+    def history(self):
+      # you have to print it and make sure that you are formatting based on the lab writeup
+      pass
