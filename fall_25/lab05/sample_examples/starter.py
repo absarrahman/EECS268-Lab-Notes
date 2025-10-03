e0e20150677bc98060d15d68d5da0cd3f798bf72
absarrahman <raman040635@gmail.com>
2025-10-03 08:53:09 -0500

    lab05: starter template
    Signed-off-by: absarrahman <raman040635@gmail.com>

---
+++ fall_25/lab05/sample_examples/starter.py
@@ -0,0 +1,26 @@
+def power(base, power):
+    # handle base case when pow is 0
+    # handle recursive case when pow is greater
+    pass
+
+
+def flu_outbreak(day):
+    # handle base case for day 1
+    # handle base case for day 2
+    # handle base case for day 3
+
+    # handle recursive case for more than day > 3
+    # hint:  It's similar to fibbonacci
+    pass
+
+def fibonacci(v):
+    # handle base case for 0 and 1
+    # handle recursive case when v > 1
+    pass
+
+def is_fibonacci(num, num_a=0, num_b=1):
+    # handle base case when num == num_a
+    # handle base case when num < num_a
+    # handle recursive case when num > num_a
+    # hint: num, num_b, num_a + num_b
+    pass
