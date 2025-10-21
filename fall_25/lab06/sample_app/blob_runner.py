cc28cad82367d5bd3339bad8ab434043916cbf2d
absarrahman <raman040635@gmail.com>
2025-10-21 12:32:44 -0500

    lab06: update sample
    Signed-off-by: absarrahman <raman040635@gmail.com>

---
+++ fall_25/lab06/sample_app/blob_runner.py
@@ -0,0 +1,34 @@
+class BlobRunner:
+    def __init__(self) -> None:
+        # initialze rows and columns
+        # initialize start row and start column
+        # set the map list
+        # set eaten count
+        pass
+        
+    
+    # starts the simulation
+    def run(self) -> None:
+        pass
+        # self.start_blob_cycle(x, y) # start the recursion at the starting point for the blob
+        # self.print_map()
+    
+    # scans around and on the tile (x,y)
+    def start_blob_cycle(self, x, y) -> None:
+        # if coord S->B
+        # elif coord P->B>Increment eaten count
+
+        # explore +Y
+
+        # explore +X
+
+        # explore -Y
+
+        # check -X
+
+       # iterate map list
+        pass
+
+    # prints out
+    def print_map(self) -> None:
+        pass
