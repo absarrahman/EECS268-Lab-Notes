271142b0173c543aa62151badf52bf9b784885f9
absarrahman <raman040635@gmail.com>
2025-11-04 15:45:13 -0600

    Update lab 7 sample
    Signed-off-by: absarrahman <raman040635@gmail.com>

---
+++ fall_25/lab07/sample_app/sample.py
@@ -0,0 +1,28 @@
+def inorder(self, current_node, closure):
+    # check whether current node is None
+    # if none -> Return
+    #
+    # Call inorder(current_node.left, closure)
+    # execute closure -> closure(current_node.entry)
+    # Call inorder(current_node.right, closure)
+    pass
+
+
+def postorder(self, current_node, closure):
+    # check whether current node is None
+    # if none -> Return
+    #
+    # Call postorder(current_node.left, closure)
+    # Call postorder(current_node.right, closure)
+    # execute closure -> closure(current_node.entry)
+    pass
+
+
+def preorder(self, current_node, closure):
+    # check whether current node is None
+    # if none -> Return
+    #
+    # execute closure -> closure(current_node.entry)
+    # Call preorder(current_node.left, closure)
+    # Call preorder(current_node.right, closure)
+    pass
